75dc050e6e2ed0d094ad749a77e99dba55119eb2
J Lakshmi Vivek <jlakshmivivek17@gmail.com>
2025-02-05 21:31:45 -0500

    first Commit

---
+++ src/main/java/com/example/journalApp/controller/JournalEntryController.java
@@ -0,0 +1,31 @@
+package com.example.journalApp.controller;
+
+import com.example.journalApp.entity.JournalEntry;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/journal")
+public class JournalEntryController {
+
+    private Map<Long, JournalEntry> journalEntries = new HashMap<>();
+
+    @GetMapping
+    public List<JournalEntry> getAll(){
+        return new ArrayList<>(journalEntries.values());
+    }
+
+    @PostMapping
+    public void createEntry(){
+
+    }
+
+}
